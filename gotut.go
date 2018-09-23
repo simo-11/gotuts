55f010fb7a0ee233dd5f6016ab82e74f100ce47b
Simo Nikula <simonikula@gmail.com>
2018-09-23 14:20:41 +0300

    url testing

--- gotut.go
+++ gotut.go
@@ -1,9 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
 
 func main() {
-	for i := 0; i < 3; i++ {
-		fmt.Printf("Welcome to Go %d\n", i)
+	f, err := os.OpenFile("d:/logs/gotuts.log",
+		os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	log.SetOutput(f)
+	defer f.Close()
+	url := "http://ftp.funet.fi"
+	for {
+		resp, err := http.Get(url)
+		if err != nil {
+			log.Print(url, " failed: ", err)
+		}
+		resp.Body.Close()
+		time.Sleep(1 * time.Second)
 	}
 }
