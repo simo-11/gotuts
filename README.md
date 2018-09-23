1294bbd1b658e524a35e3c4457771ca111b910ed
Simo Nikula <simonikula@gmail.com>
2018-09-23 19:17:40 +0300

    lumberjack urls

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ Getting knowledge on golang and atom
 ## go
 https://golang.org/doc/install?download=go1.11.windows-amd64.msi to d:\apps\Go
 
+## lumberjack (and others)
+:\GitHub\gotuts>go get gopkg.in/yaml.v1
+
 ## Atom
 https://atom.io/ to AppData\Local\atom\
 
--- gotut.go
+++ gotut.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"os"
 	"time"
+
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 func main() {
@@ -13,15 +15,29 @@ func main() {
 	if err != nil {
 		log.Fatalf("error opening file: %v", err)
 	}
-	log.SetOutput(f)
+	log.SetOutput(&lumberjack.Logger{
+		Filename:   "d:/logs/gotuts.log",
+		MaxSize:    10, // megabytes
+		MaxBackups: 3,
+		MaxAge:     28,    //days
+		Compress:   false, // disabled by default
+	})
+	timeout := time.Duration(10 * time.Second)
+	client := &http.Client{
+		Timeout: timeout,
+	}
 	defer f.Close()
-	url := "http://ftp.funet.fi"
+	urls := []string{"http://ftp.funet.fi",
+		"http://157.24.8.10"}
 	for {
-		resp, err := http.Get(url)
-		if err != nil {
-			log.Print(url, " failed: ", err)
+		for _, url := range urls {
+			resp, err := client.Get(url)
+			if err != nil {
+				log.Print(url, " failed: ", err)
+			} else {
+				resp.Body.Close()
+			}
 		}
-		resp.Body.Close()
 		time.Sleep(1 * time.Second)
 	}
 }
